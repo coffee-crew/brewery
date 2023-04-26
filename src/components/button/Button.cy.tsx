0a3a474071aa154bfaa73442896a441d7d1c64fc
Arjun Puri <arjun_261094@hotmail.com>
2023-04-26 15:38:26 +0200

    WIP

---
+++ src/components/button/Button.cy.tsx
@@ -0,0 +1,13 @@
+import { Button } from './Button';
+
+describe('<Button />', () => {
+  it('Renders default button', () => {
+    cy.mount(<Button label="Default" />);
+  });
+});
+
+describe('<Button />', () => {
+  it('Renders primary button', () => {
+    cy.mount(<Button label="Primary" isPrimary />).should('have.class', 'storybook-button--primary');
+  });
+});
