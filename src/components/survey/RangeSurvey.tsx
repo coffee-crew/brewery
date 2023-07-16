1fd4545a6e110068091bda28dbca7f3126c8c472
github-ambertaal <amber.taal@gmail.com>
2023-07-16 11:36:40 +0200

    feat(range survey): calculated range and given the range render out buttons

--- src/components/survey/RangeSurvey.tsx
+++ src/components/survey/RangeSurvey.tsx
@@ -38,12 +38,11 @@ export const RangeSurvey = ({ description, max, min, title, step }: RangeSurveyP
 
   // We now receive a min and max object, containing values. Let's calculate the range between these values, INCLUDING the values, and store that into a range variable.
   // To calculate range: create an array of numbers, start from the lowest number, iterating up to the highest number
-  // AT: so for min: 0, max: 10, the result is [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10] and for min: 2, max: 12, the result is [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
   const range = (start: number, stop: number, step: number) =>
     Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
 
-  console.log(range(min.number, max.number, step))
+  let surveyRange = range(min.number, max.number, step)
 
   return (
     <div className="fixed bottom-0 right-0 z-50">
@@ -69,99 +68,26 @@ export const RangeSurvey = ({ description, max, min, title, step }: RangeSurveyP
             className="max-w-lg flex-col content-center items-center rounded border-4 bg-white"
           >
             <div className="m-2">
-              <b>{title}</b>
+              <p>{title}</p>
+              <p>{description}</p>
             </div>
             <div className="m-2">
               {/* We know what our range is. Given our range, render out X-amount of buttons, effectively decreasing a BUNCH of repetitive code. */}
               {/* range amount == amount of buttons */}
-              <button
-                type="button"
-                value="0"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                0
-              </button>
-              <button
-                type="button"
-                value="1"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                1
-              </button>
-              <button
-                type="button"
-                value="2"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                2
-              </button>
-              <button
-                type="button"
-                value="3"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                3
-              </button>
-              <button
-                type="button"
-                value="4"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                4
-              </button>
-              <button
-                type="button"
-                value="5"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                5
-              </button>
-              <button
-                type="button"
-                value="6"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                6
-              </button>
-              <button
-                type="button"
-                value="7"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                7
-              </button>
-              <button
-                type="button"
-                value="8"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                8
-              </button>
-              <button
-                type="button"
-                value="9"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                9
-              </button>
-              <button
-                type="button"
-                value="10"
-                onClick={handleRatingClick}
-                className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
-              >
-                10
-              </button>
+              {surveyRange.map((number) => {
+                return (
+                  <>
+                    <button
+                      type="button"
+                      value={number}
+                      onClick={handleRatingClick}
+                      className="m-2 rounded-sm bg-gray-200 p-2 text-black hover:bg-gray-400 focus:bg-gray-400 focus:outline-none active:bg-gray-400"
+                    >
+                      {number}
+                    </button>
+                  </>
+                )
+              })}
               <div className="flow-root">
                 <div className="float-left m-2">{min.label}</div>
                 <div className="float-right m-2">{max.label}</div>
